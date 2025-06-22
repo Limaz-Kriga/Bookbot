76ab5346b0c8b24e4328fb874762c8669f410de7
Limaz-Kriga <cato.rusten@gmail.com>
2025-06-22 12:40:40 +0100

    B: printing report fuction

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from stats import word_count
 from stats import character_count
+from stats import create_dict
+from stats import sort_by_high
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
+def print_report(amount, sorted_list):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {amount} total words")
+    print("--------- Character Count -------")
+    for dict_item in sorted_list:
+        if dict_item["char"].isalpha():
+            print(f"{dict_item["char"]}: {dict_item["num"]}")
+    print("============= END ===============")
+
+
+
+
 def main():
     filepath = "./books/frankenstein.txt"
    # contents = get_book_text(filepath)
     amount = word_count(filepath)
     characters = character_count(filepath)
+    list_dict = create_dict(characters)
+    sorted_list = sort_by_high(list_dict)
+    print_report(amount,sorted_list)
    # print(contents)
-    print(f"{amount} words found in the document")
-    print(characters)
+   # print(f"{amount} words found in the document")
+   # print(characters)
     
-
 
 main()
 
--- stats.py
+++ stats.py
@@ -13,7 +13,22 @@ def character_count(filepath):
             else:
                     char_count[i] = 1
     return char_count
-        
+
+def create_dict(input_dict):
+     output_dict = []
+     for key in input_dict:
+          output_dict.append ({"char": key, "num": input_dict[key]})
+     return output_dict
+     
+def sort_by_high(input_dict):
+     def get_num(char_count):
+          return char_count["num"]
+     input_dict.sort(key=get_num, reverse=True)
+     return input_dict
+
+     
+    
+             
 
 
 
